ae75d7927031a42df1c99cd405a4f17b65c37515
Leonardo F Petrauskas <leonardopetrauskas@hotmail.com>
2019-11-22 00:37:55 -0300

    Botao Editar

--- seguranca/src/components/ListComponent.jsx
+++ seguranca/src/components/ListComponent.jsx
@@ -8,8 +8,42 @@ class ListComponent extends React.Component {
         super(props);
         this.state = {
             lista: props.lista,
-            body: []
-        }
+            body: [],
+            editWindow: null,
+        };
+        this.editar = () => { console.log('asd'); this.setState({ editWindow: this.state.editWindow ? null : <div>EAEAEAEAEAE EAEAEAEAEAE EAEAEAEAEAE EAEAEAEAEAE </div> }) }
+    }
+
+    // componentDidMount() {
+    //     (async () => {
+    //         try {
+    //             let { lista } = this.state;
+    //             let propriedades = null, body = [];
+    //             if (lista && lista.length > 0) {
+    //                 let middle = [], body = [];
+    //                 propriedades = Object.keys(lista[0]);
+    //                 let colunas = await getParams(propriedades.toString());
+    //                 let top = <div key='titulo'>{colunas.map(c => <div key={c.desc_param}>{c.valor_param}</div>)}</div>;
+
+    //                 for (let i = 0; i < lista.length; i++) {
+    //                     for (let j = 0; j < propriedades.length; j++) {
+    //                         middle.push(<div key={key++}>{lista[i][propriedades[j]]}</div>)
+    //                     }
+    //                     body.push(<div key={key++}>{middle}</div>)
+    //                 }
+    //                 body.unshift(top)
+    //                 this.setState({ body });
+    //             }
+    //         } catch (error) {
+    //             console.log(error)
+    //             throw error;
+    //         }
+    //     })();
+    // }
+
+    addEditButton = (codigo) => {
+        return <td key={key++}><button onClick={this.editar}>Editar {codigo} </button></td>
+
     }
 
     componentDidMount() {
@@ -20,19 +54,20 @@ class ListComponent extends React.Component {
                 if (lista && lista.length > 0) {
                     propriedades = Object.keys(lista[0]);
                     let colunas = await getParams(propriedades.toString());
-                    let top = <div key='titulo' className='title'>{colunas.map(c => <div className='cell' key={c.desc_param}>{c.valor_param}</div>)}</div>;
-                    let middle = [], body = [];
+                    let top = <tr className='titulo' key='titulo'>{colunas.map(c => <td key={c.desc_param}>{c.valor_param}</td>)}<td>Editar</td></tr>;
+
                     for (let i = 0; i < lista.length; i++) {
+                        let middle = [];
                         for (let j = 0; j < propriedades.length; j++) {
-                            middle.push(<div className='cell' key={key++}>{lista[i][propriedades[j]]}</div>)
+                            middle.push(<td key={key++}>{lista[i][propriedades[j]]}</td>)
                         }
-                        body.push(<div className='row' key={key++}>{middle}</div>)
+                        middle.push(this.addEditButton(lista[i][propriedades[0]]))
+                        body.push(<tr key={key++}>{middle}</tr>)
                     }
                     body.unshift(top)
                     this.setState({ body });
                 }
             } catch (error) {
-                console.log(error)
                 throw error;
             }
         })();
@@ -41,7 +76,12 @@ class ListComponent extends React.Component {
     render() {
         return (
             <React.Fragment>
-                {this.state.body}
+                {this.state.editWindow}
+                <table>
+                    <tbody className="grid">
+                        {this.state.body}
+                    </tbody>
+                </table>
             </React.Fragment>
         );
     }
